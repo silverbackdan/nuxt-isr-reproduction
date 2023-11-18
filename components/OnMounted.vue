ca079255047c66d16ea50569a40c72c2cb437ff7
silverbackdan <daniel@silverback.is>
2023-11-18 16:01:16 +0000

    Demo setup with same content and different rendering modes

---
+++ components/OnMounted.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import {onMounted, ref} from "vue";
+
+const { payload: { prerenderedAt } } = useNuxtApp()
+const onMountedValue = ref(false)
+
+onMounted(() => {
+  onMountedValue.value = true
+})
+</script>
+<template>
+  <div>
+    <div>
+      <NuxtLink to="/">Home (SSR)</NuxtLink> |
+      <NuxtLink to="/isr">ISR</NuxtLink> |
+      <NuxtLink to="/prerendered">Pre-Rendered</NuxtLink>
+    </div>
+    <pre>Has the onMounted value updated? `{{ onMountedValue }}`
+prerenderedAt value? `{{ prerenderedAt }}`</pre>
+  </div>
+</template>
