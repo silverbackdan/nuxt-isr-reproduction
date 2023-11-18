545b09a2e952506f1ec4e1ab5edf8da6fd90dac4
silverbackdan <daniel@silverback.is>
2023-11-18 16:12:02 +0000

    Also output value if immediate watcher is called

--- components/OnMounted.vue
+++ components/OnMounted.vue
@@ -3,9 +3,20 @@ import {onMounted, ref} from "vue";
 
 const { payload: { prerenderedAt } } = useNuxtApp()
 const onMountedValue = ref(false)
+const watcherCalled = ref()
+
+const methods = {
+  updateWatcherCalled() {
+    watcherCalled.value = true
+  }
+}
 
 onMounted(() => {
   onMountedValue.value = true
+  watcherCalled.value = false
+  watch(onMountedValue, methods.updateWatcherCalled, {
+    immediate: true
+  })
 })
 </script>
 <template>
@@ -16,6 +27,7 @@ onMounted(() => {
       <NuxtLink to="/prerendered">Pre-Rendered</NuxtLink>
     </div>
     <pre>Has the onMounted value updated? `{{ onMountedValue }}`
+watcherCalled value? `{{ watcherCalled }}`
 prerenderedAt value? `{{ prerenderedAt }}`</pre>
   </div>
 </template>
